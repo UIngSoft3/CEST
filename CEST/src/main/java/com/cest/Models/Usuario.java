3ed9cb9f4ec68d84d0559ba5c86b033a59886046
luistrejos <34162422+luistrejos@users.noreply.github.com>
2018-09-14 09:07:34 -0500

    Pruebas de Ubicacion

--- CEST/src/main/java/com/cest/Models/Usuario.java
+++ CEST/src/main/java/com/cest/Models/Usuario.java
@@ -2,6 +2,8 @@ package com.cest.Models;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 
 @Entity
 public class Usuario {
