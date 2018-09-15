9267f7b2f60c04589b6fe41fc6a0bb89614bd78a
luistrejos <34162422+luistrejos@users.noreply.github.com>
2018-09-15 10:28:09 -0500

    .

--- CEST/src/test/java/com/cest/ExtintorControllerPostRegistrar.java
+++ CEST/src/test/java/com/cest/ExtintorControllerPostRegistrar.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.servlet.ModelAndView;
 import com.cest.Controllers.ExtintorController;
@@ -24,6 +25,7 @@ public class ExtintorControllerPostRegistrar {
 	Extintor extintor = new Extintor();
 	
 	@Before
+	@Sql()
 	public void setup() {		
 		extintor.setIdelemento(1011);
 		extintor.setTamanio("15 Libras");
