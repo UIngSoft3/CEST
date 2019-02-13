fbd4f761631980ca546201651b79844ef6e07132
luistrejos <34162422+luistrejos@users.noreply.github.com>
2019-02-13 12:02:13 -0500

    .

--- CEST/src/main/resources/data.sql
+++ CEST/src/main/resources/data.sql
@@ -1,2 +1,11 @@
 delete from Usuario;
+insert into Usuario values ('user','user');
+insert into Usuario values ('luis','luis');
 insert into Usuario values ('cest','cest');
+
+
+delete from Empresa;
+INSERT INTO `empresa` (`nit`, `direccion`, `nombre`, `telefono`) VALUES
+(12, 'Calle 61 # 11 - 29', 'Apaga Fuegos', 45083),
+(11, 'Carrera 73A # 2 - 45', 'Fuego y Familia', 23622),
+(10, 'Km 22 via Magdalena', 'Bomberman', 88400);
