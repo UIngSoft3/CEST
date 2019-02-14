4caf59d707c899e885f5c6923d82004bf9ce76b0
luistrejos <luis.1701321107@ucaldas.edu.co>
2019-02-14 09:06:21 -0500

    .

--- CEST/src/main/resources/data.sql
+++ CEST/src/main/resources/data.sql
@@ -1,11 +1,99 @@
-delete from usuario;
+
 insert into usuario values ('user','user');
-insert into usuario values ('luis','luis');
 insert into usuario values ('cest','cest');
+insert into usuario values ('luis','luis');
 
 
-delete from empresa;
-INSERT INTO `empresa` (`nit`, `direccion`, `nombre`, `telefono`) VALUES
+INSERT INTO bloque VALUES
+(1, 'C', 'Edificio Central', 1),
+(1, 'B', 'Edificio Central', 1),
+(1, 'A', 'Edificio Administrativo', 1),
+(1, 'D', 'Edificio del Parque', 1),
+(1, 'E', 'Edificio Laboratorios', 1),
+(1, 'S', 'Edificio Biofilo', 1),
+(1, 'U', 'Edificio Bicentenario', 1),
+(2, 'H', 'Edificio Palogrande', 2),
+(2, 'I', 'Edificio Palogrande', 2),
+(2, 'J', 'Edificio Palogrande', 2),
+(2, 'O', 'Edificio Palogrande', 2),
+(3, 'N', 'Edificio Bellas Artes', 3),
+(4, 'K', 'Edificio Ciencias para la Salud', 4),
+(4, 'M', 'Edificio Ciencias para la Salud', 4),
+(4, 'L', 'Edificio Ciencias para la Salud', 4),
+(5, 'F', 'Edificio Ciencias Agropecuarias', 5),
+(5, 'G', 'Edificio Ciencias Agropecuarias', 5);
+
+INSERT INTO contrato VALUES
+(999, 'Recarga de 5 extintores', '2018-05-16', 500000, 12),
+(888, 'Recarga Extintores', '2018-05-23', 400000, 11);
+
+INSERT INTO elemento VALUES
+(1606, 999, 1234, 'C', 1),
+(13, 999, 1234, 'C', 1),
+(2431, 999, 1234, 'C', 1),
+(2020, 888, 9876, 'B', 2),
+(3030, 888, 9876, 'H', 1),
+(9090, 999, 1234, 'C', 4),
+(8080, 999, 1234, 'C', 3),
+(600, 999, 1234, 'C', 4),
+(2000, 999, 1234, 'D', 2);
+
+INSERT INTO empresa VALUES
 (12, 'Calle 61 # 11 - 29', 'Apaga Fuegos', 45083),
 (11, 'Carrera 73A # 2 - 45', 'Fuego y Familia', 23622),
 (10, 'Km 22 via Magdalena', 'Bomberman', 88400);
+
+INSERT INTO encargado VALUES
+(1234, 'Pepito Perez'),
+(9876, 'Juan Gomez'),
+(12345678, 'Anita Velez'),
+(10279174, 'Jose Fernando');
+
+INSERT INTO extintor VALUES
+(1606, '3', 'Activo', '2018-05-16', '2021-05-16', '10 Libras', 1606, 'ABC'),
+(13, '4', 'Baja', '2018-05-21', '2022-05-21', '370 Gramos', 13, 'CO2'),
+(2431, '2', 'Mantenimiento', '2018-05-22', '2020-05-22', '15 Libras', 2431, 'ABC'),
+(2020, '4', 'Activo', '2018-05-23', '2022-05-23', '20 Libras', 2020, 'CO2'),
+(3030, '2', 'Vencido', '2018-05-23', '2020-05-23', '2,5 Litros', 3030, 'ABC'),
+(9090, '2', 'Activo', '2018-05-24', '2020-05-24', '15 Libras', 9090, 'ABC'),
+(8080, '3', 'Activo', '2018-05-25', '2021-05-25', '150 Litros', 8080, 'ABC'),
+(600, '2', 'Activo', '2018-05-28', '2020-05-28', '150 Litros', 600, 'ABC');
+
+INSERT INTO camilla VALUES
+(2000, 1234, 'Lona', 2000);
+
+INSERT INTO fichatecnica VALUES
+('ABC', 'ABC', NULL),
+('CO2', 'Dióxodo de Carbono', NULL);
+
+INSERT INTO piso VALUES
+('C', 1, 1, 'C'),
+('C', 2, 1, 'C'),
+('C', 3, 1, 'C'),
+('C', 4, 1, 'C'),
+('B', 1, 1, 'B'),
+('B', 2, 1, 'B'),
+('B', 3, 1, 'B'),
+('B', 4, 1, 'B'),
+('A', 1, 1, 'A'),
+('A', 2, 1, 'A'),
+('A', 3, 1, 'A'),
+('A', 4, 1, 'A'),
+('A', 5, 1, 'A'),
+('D', 0, 1, 'D'),
+('D', 1, 1, 'D'),
+('D', 2, 1, 'D'),
+('D', 3, 1, 'D'),
+('H', 1, 2, 'H'),
+('H', 2, 2, 'H'),
+('H', 3, 2, 'H'),
+('H', 4, 2, 'H');
+
+INSERT INTO sede VALUES
+(1, 'Central'),
+(2, 'Palogrande'),
+(3, 'Bellas Artes'),
+(4, 'Versalles'),
+(5, 'Sancancio');
+
+
