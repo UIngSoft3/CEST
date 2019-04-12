95e1ce6199045a34cc3cd4aa331f5437ab08de99
luistrejos <luis.1701321107@ucaldas.edu.co>
2019-04-12 12:00:51 -0500

    Web service completo

--- CEST/src/main/java/com/cest/CestApplication.java
+++ CEST/src/main/java/com/cest/CestApplication.java
@@ -3,9 +3,10 @@ package com.cest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class CestApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CestApplication.class, args);
 	}
--- CEST/src/main/java/com/cest/Services/ExtintorService.java
+++
@@ -1,8 +0,0 @@
-package com.cest.Services;
-
-import com.cest.Models.Extintor;
-
-public interface ExtintorService {
-	
-	public Extintor update(Extintor extintor);
-}
